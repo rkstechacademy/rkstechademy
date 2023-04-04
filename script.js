5c464272671c181ac017ac44e8120b0a1347d92a
RKsTechAdemy <128132850+rkstechacademy@users.noreply.github.com>
2023-04-04 16:09:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ function show() {
     document.querySelectorAll('.hr').forEach(el => el.classList.toggle('active'));
     
 }
+var counterContainer = document.querySelector(".website-counter");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+
+// Adding onClick event listener
+resetButton.addEventListener("click", () => {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+  counterContainer.innerHTML = visitCount;
+});
